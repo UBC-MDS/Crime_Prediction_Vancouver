ed35666e5beaed4e2d0643c4220415c04c01df8e
Thomas Siu <thomas.siu@gmail.com>
2021-11-25 13:16:16 -0800

    Add pipeline shell script for easier usage

---
+++ pipeline.sh
@@ -0,0 +1,2 @@
+python src/download_data.py --url=https://geodash.vpd.ca/opendata/crimedata_download/crimedata_csv_all_years.zip?disclaimer=on --file_path=data/raw --zip_file_name=crimedata_csv_all_years.csv
+python src/pre_process_data.py --input_path=data/raw/crimedata_csv_all_years.csv --out_path=data/processed/
