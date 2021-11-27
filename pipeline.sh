7973895b31737b9f0fb56d198d44369ec31a356b
Thomas Siu <thomas.siu@gmail.com>
2021-11-27 14:12:10 -0800

    Updated pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -10,11 +10,23 @@
 #rm src/crime_vancouver_eda.html
 #rm -rf results/
 
-Rscript -e "rmarkdown::render('README.Rmd')"
+# download the data
 python src/download_data.py --url=https://geodash.vpd.ca/opendata/crimedata_download/crimedata_csv_all_years.zip?disclaimer=on --file_path=data/raw --zip_file_name=crimedata_csv_all_years.csv
+
+# split the data into train and test
 python src/split_data.py --input_path=data/raw/crimedata_csv_all_years.csv --out_path=data/processed/  --graph_path=src/figure-preprocess/
+
+# perform EDA
 python src/crime_vancouver_eda.py --input_path=data/processed/training_df.csv --out_dir=src/figure-eda/
+
+# render EDA figures
 Rscript -e "rmarkdown::render('src/crime_vancouver_eda.Rmd')"
+
+# create pre-processor for column transformation
 python src/pre_process_data.py --out_path=data/processed/
+
+# fit and tune the model
 python src/modelling.py --input_path=data/processed/ --out_path=results/
+
+# render final report
 Rscript -e "rmarkdown::render('doc/vancouver_crime_predict_report.Rmd')"
